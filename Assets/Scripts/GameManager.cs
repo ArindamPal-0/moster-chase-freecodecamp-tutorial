5ee3a393ad379662f0d04f1a8214c7a546a02948
Arindam Pal <arindampal2000@gmail.com>
2022-08-12 23:48:37 +0530

    scene navigation, player selection, player spawning, restart and home
    - navigating between scenes
    - selecting among players
    - static variable and singleton pattern
    - delegates and subscribing to sceneLoad event to spawn player
    - restart and homeButton UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -25,6 +26,24 @@ public class GameManager : MonoBehaviour
         } else 
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishLoading;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishLoading;
+    }
+
+    void OnLevelFinishLoading(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "Gameplay")
+        {
+            Instantiate(characters[CharIndex]);
         }
     }
 }// class
